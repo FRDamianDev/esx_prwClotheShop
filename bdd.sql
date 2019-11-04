0b67736ade0fd6937dee897a9c44e24e1f030cdc
Parow <rya4an@gmail.com>
2019-11-04 21:46:32 +0100

    Update bdd.sql

--- bdd.sql
+++ bdd.sql
@@ -1,19 +1,15 @@
--- --------------------------------------------------------
--- Hôte :                        localhost
--- Version du serveur:           5.7.24 - MySQL Community Server (GPL)
--- SE du serveur:                Win64
--- HeidiSQL Version:             10.2.0.5599
--- --------------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS `user_contacts` (
+CREATE TABLE IF NOT EXISTS `user_accessories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `identifier` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `number` int(11) NOT NULL,
+  `mask` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `label` varchar(50) COLLATE utf8mb4_bin DEFAULT 'Masque',
+  `type` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `index` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-
+-- Les données exportées n'étaient pas sélectionnées.
+-- Listage de la structure de la table lscity. user_tenue
 CREATE TABLE IF NOT EXISTS `user_tenue` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `identifier` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
@@ -21,8 +17,3 @@ CREATE TABLE IF NOT EXISTS `user_tenue` (
   `label` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
--- Les données exportées n'étaient pas sélectionnées.
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
